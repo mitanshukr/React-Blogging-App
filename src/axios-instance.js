eaa191f0031e2dea9f7256f0c4bdf6818c32fdd7
Mitanshu Kumar <mitanshukr@yahoo.com>
2023-01-27 12:12:52 +0530

    updated backend url from heroku to render.com

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://immune-ink.herokuapp.com/",
+  baseURL: "https://immune-ink-server.onrender.com/",
 });
 
 export default instance;
